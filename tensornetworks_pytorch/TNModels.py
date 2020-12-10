5c1b3c2050e512ac7dbf715ace51ab41d90c32df
jpalucci <jonathan.palucci@mail.mcgill.ca>
2020-12-09 22:11:35 -0500

    Attempt at implementing MPS log stable

--- tensornetworks_pytorch/TNModels.py
+++ tensornetworks_pytorch/TNModels.py
@@ -11,15 +11,18 @@ class PosMPS(TTrain):
 
     def __init__(
             self, dataset, d, D, 
-            homogeneous=True, w_randomization=None, verbose=False):
+            homogeneous=True, w_randomization=None, log_stability=True, verbose=False):
         super().__init__(
             dataset, d, D, dtype=torch.float, 
             homogeneous=homogeneous, w_randomization=w_randomization, verbose=verbose)
+        self.log_stability = log_stability
         self.name = "Positive MPS"
         if homogeneous:
             self.name += ", Homogeneous"
         else:
             self.name += ", Non-homogeneous"
+        if log_stability:
+            self.name += " + log_stability"
 
 
     def _logprob(self, x):
@@ -31,11 +34,17 @@ class PosMPS(TTrain):
         Returns:
             logprob (torch.Tensor): size []
         """
-
-        unnorm_prob = self._contract_at(x)
-        normalization = self._contract_all()
-        logprob = unnorm_prob.log() - normalization.log()
+        if self.log_stability:
+            unnorm_logprob = self._log_contract_at(x)
+            log_normalization = self._log_contract_all()
+            logprob = unnorm_logprob - log_normalization
+            # print(output, unnorm_prob, normalization, logprob)
+        else:
+            unnorm_prob = self._contract_at(x)
+            normalization = self._contract_all()
+            logprob = unnorm_prob.log() - normalization.log()
         return logprob
+
 
     def _contract_at(self, x):
         """Contract network at particular values in the physical dimension,
@@ -97,6 +106,84 @@ class PosMPS(TTrain):
         # if self.verbose:
         #     print("contract_all", output)
         return output
+
+    def _log_contract_at(self, x):
+        """Contract network at particular values in the physical dimension,
+        for computing probability of x.
+        """
+        # repeat the core seqlen times
+        if self.homogeneous:
+            # repeat the core seqlen times
+            w = self.core[None].repeat(self.seqlen, 1, 1, 1)
+        else:
+            w = self.core
+        w2 = w.square()
+        left_boundary2 = self.left_boundary.square()
+        right_boundary2 = self.right_boundary.square()
+        Z = self.vec_norm(left_boundary2)
+        contractor_unit = left_boundary2 / Z
+        accumulated_lognorm = Z.log()
+        # contract the network, from the left boundary through to the last core
+        #contracting_tensor = left_boundary2
+        for i in range(self.seqlen):
+            contractor_temp = torch.einsum(
+                'i, ij -> j',
+                contractor_unit,
+                w2[i, x[i], :, :])
+            Z = self.vec_norm(contractor_temp)
+            contractor_unit = contractor_temp / Z
+            accumulated_lognorm += Z.log()
+            if contractor_unit.min() < 0:
+                print("contraction < 0")
+                print(w.min())
+        # contract the final bond dimension
+        output = torch.einsum(
+            'i, i ->', contractor_unit, right_boundary2)
+        logprob = accumulated_lognorm + output.log()
+        # if self.verbose:
+        #     print("contract_at", output)
+        if output < 0:
+            print("output of contract_at < 0")
+        return logprob
+
+    def _log_contract_all(self):
+        """Contract network with a copy of itself across physical index,
+        for computing norm.
+        """
+        # repeat the core seqlen times
+        if self.homogeneous:
+            # repeat the core seqlen times
+            w = self.core[None].repeat(self.seqlen, 1, 1, 1)
+        else:
+            w = self.core
+        w2 = w.square()
+        left_boundary2 = self.left_boundary.square()
+        right_boundary2 = self.right_boundary.square()
+        Z = self.vec_norm(left_boundary2)
+        contractor_unit = left_boundary2 / Z
+        accumulated_lognorm = Z.log()
+        # first, left boundary contraction
+        # (note: if real-valued conj will have no effect)
+        contractor_temp = torch.einsum(
+            'j, ijk -> k', contractor_unit, w2[0, :, :, :])
+        Z = self.vec_norm(contractor_temp)
+        contractor_unit = contractor_temp / Z
+        accumulated_lognorm += Z.log()
+        # contract the network
+        for i in range(1, self.seqlen):
+            contractor_temp = torch.einsum(
+                'j, ijk -> k',
+                contractor_unit,
+                w2[i, :, :, :])
+            Z = self.vec_norm(contractor_temp)
+            contractor_unit = contractor_temp / Z
+            accumulated_lognorm += Z.log()
+        # contract the final bond dimension with right boundary vector
+        output = torch.dot(contractor_unit, right_boundary2)
+        logprob = accumulated_lognorm + output.log()
+        # if self.verbose:
+        #     print("contract_all", output)
+        return logprob
 
     # def _contract_at_temp(self, x):
     #     """Contract network at particular values in the physical dimension,
